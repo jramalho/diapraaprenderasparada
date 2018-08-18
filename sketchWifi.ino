812c57560397ed89b299b8e36ae9f4d6eab0cebd
Jonathan Ramalho <totoramalho@gmail.com>
2018-08-17 21:27:06 -0300

    Codigo conectando wifi no node

---
+++ sketchWifi.ino
@@ -0,0 +1,20 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "teste";
+const char* password = "12345670";
+void setup() {
+    Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid,password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
